c089957ad489e2ff2f4ec55dae5f7db773797b08
dengjianbin@lesdo.cn <dengjianbin@lesdo.cn>
2019-06-11 16:07:20 +0800

    1、增加MySpring 启动类 2、增加MySpring 测试主类

---
+++ src/main/java/framework/starter/MySpringApplication.java
@@ -0,0 +1,16 @@
+package framework.starter;
+
+/**
+ * @author : xiaoheshang
+ *
+ * @time : 2019/6/11 - 4:04 PM
+ * @Param :
+ * @function : MySpring 启动类
+ */
+public class MySpringApplication {
+
+    public static void run(Class<?> classes, String[] args){
+        System.out.println("Hello MySpringApplication");
+    }
+
+}
---
+++ src/main/java/test/MainTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import framework.starter.MySpringApplication;
+
+/**
+ * @author : xiaoheshang
+ *
+ * @time : 2019/6/11 - 4:05 PM
+ * @Param :
+ * @function : 日常测试类
+ */
+public class MainTest {
+
+    public static void main(String[] args){
+        MySpringApplication.run(MainTest.class, args);
+    }
+
+}
